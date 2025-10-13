cc231fa85dac50cfc06ef058b98df0b614ff9aff
Dennis <dennisjemison01@gmail.com>
2025-10-13 00:37:32 -0500

    Modal edits

---
+++ scripts/index.js
@@ -0,0 +1,23 @@
+const editProfileBtn = document.querySelector(".profile__edit-button");
+const editProfileModal = document.querySelector("#edit-profile-modal");
+const editCloseBtn = editProfileModal.querySelector(".modal__close-button");
+const newPostBtn = document.querySelector(".profile__post-button")
+const newPostModal = document.querySelector("#new-post-nodal")
+const newCloseBtn = newPostModal.querySelector(".modal__close-button")
+
+editProfileBtn.addEventListener("click", function() {
+  editProfileModal.classList.add("modal_is-opened")
+});
+
+editCloseBtn.addEventListener("click", function() {
+  editProfileModal.classList.remove("modal_is-opened")
+});
+
+
+newPostBtn.addEventListener("click", function(){
+  newPostModal.classList.add("modal_is-opened")
+});
+
+newCloseBtn.addEventListener("click", function(){
+  newPostModal.classList.remove("modal_is-opened")
+});
